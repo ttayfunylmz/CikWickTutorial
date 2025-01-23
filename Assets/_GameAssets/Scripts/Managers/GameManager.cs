e8f6e151ca55ddd430ab1b3d0f8dce4665849edd
Tayfun Yılmaz <tyfnylmz201974@gmail.com>
2025-01-24 02:08:00 +0300

    v0.20 Cat Animations & Lose Situations Added

--- Assets/_GameAssets/Scripts/Managers/GameManager.cs
+++ Assets/_GameAssets/Scripts/Managers/GameManager.cs
@@ -9,8 +9,10 @@ public class GameManager : MonoBehaviour
     public event Action<GameState> OnGameStateChanged;
 
     [Header("References")]
+    [SerializeField] private CatController _catController;
     [SerializeField] private EggCounterUI _eggCounterUI;
     [SerializeField] private WinLoseUI _winLoseUI;
+    [SerializeField] private PlayerHealthUI _playerHealthUI;
 
     [Header("Settings")]
     [SerializeField] private int _maxEggCount = 5;
@@ -27,7 +29,14 @@ public class GameManager : MonoBehaviour
 
     private void Start() 
     {
-        HealthManager.Instance.OnPlayerDeath += HealthManager_OnPlayerDeath;    
+        HealthManager.Instance.OnPlayerDeath += HealthManager_OnPlayerDeath;
+        _catController.OnCatCatched += CatController_OnCatCatched;  
+    }
+
+    private void CatController_OnCatCatched()
+    {
+        _playerHealthUI.AnimateDamageForAll();
+        StartCoroutine(OnGameOver());
     }
 
     private void HealthManager_OnPlayerDeath()
