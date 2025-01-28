bb7eae255384b5773eacc07b7087b2b64c66a53e
Tayfun Yılmaz <tyfnylmz201974@gmail.com>
2025-01-28 17:44:58 +0300

    v0.28 Settings Menu Improved

--- Assets/_GameAssets/Scripts/UI/SettingsUI.cs
+++ Assets/_GameAssets/Scripts/UI/SettingsUI.cs
@@ -1,3 +1,4 @@
+using System;
 using DG.Tweening;
 using MaskTransitions;
 using UnityEngine;
@@ -16,10 +17,19 @@ public class SettingsUI : MonoBehaviour
     [SerializeField] private Button _resumeButton;
     [SerializeField] private Button _mainMenuButton;
 
+    [Header("Sprites")]
+    [SerializeField] private Sprite _musicActiveSprite;
+    [SerializeField] private Sprite _musicPassiveSprite;
+    [SerializeField] private Sprite _soundActiveSprite;
+    [SerializeField] private Sprite _soundPassiveSprite;
+
     [Header("Settings")]
     [SerializeField] private float _animationDuration;
 
     private Image _blackBackgroundImage;
+
+    private bool _isMusicActive;
+    private bool _isSoundActive;
 
     private void Awake() 
     {
@@ -34,6 +44,25 @@ public class SettingsUI : MonoBehaviour
             AudioManager.Instance.Play(SoundType.TransitionSound);
             TransitionManager.Instance.LoadLevel(Consts.SceneNames.MENU_SCENE);
         });
+
+        _musicButton.onClick.AddListener(OnMusicButtonClicked);
+        _soundButton.onClick.AddListener(OnSoundButtonClicked);
+    }
+
+    private void OnSoundButtonClicked()
+    {
+        AudioManager.Instance.Play(SoundType.ButtonClickSound);
+        _isSoundActive = !_isSoundActive;
+        _soundButton.image.sprite = _isSoundActive ? _soundActiveSprite : _soundPassiveSprite;
+        AudioManager.Instance.SetSoundEffectsMute(!_isSoundActive);
+    }
+
+    private void OnMusicButtonClicked()
+    {
+        AudioManager.Instance.Play(SoundType.ButtonClickSound);
+        _isMusicActive = !_isMusicActive;
+        _musicButton.image.sprite = _isMusicActive ? _musicActiveSprite : _musicPassiveSprite;
+        BackgroundMusic.Instance.SetMusicMute(!_isMusicActive);
     }
 
     private void OnSettingsButtonClicked()
